de7b66b634049184d860391edcb7098b9e494143
ldmmc <128253869+ldmmc@users.noreply.github.com>
2024-03-14 17:36:03 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2023-10-01', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://mmcha.top' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -14,12 +14,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'matmatcha.fun',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: '实时体验自部署镜像',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '个人生活',
+  HEO_HERO_TITLE_2: '记录与分享',
+  HEO_HERO_TITLE_3: 'mmcha.top',
+  HEO_HERO_TITLE_4: ' ',
+  HEO_HERO_TITLE_5: '体验我的自部署镜像',
+  HEO_HERO_TITLE_LINK: 'https://www.mmcha.top/links',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
